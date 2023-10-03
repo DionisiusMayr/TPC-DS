978812afa03a31504b7b2fba44fe0fa132a55d72
Dionisius Mayr <dionisiusom@gmail.com>
2023-10-03 20:40:33 +0200

    Update data generation script to avoid the trailing pipe

--- scripts/data_generation.sh
+++ scripts/data_generation.sh
@@ -3,15 +3,20 @@
 # Avoid using relative paths here for safety.
 
 SCALE_FACTOR=1
-TARGET_DIR="/home/dionisius/Documents/academics/universities/bdma/ulb/data_warehouses/data/sc_${SCALE_FACTOR}"
-DSDGEN_PATH="/home/dionisius/Documents/academics/universities/bdma/ulb/data_warehouses/tpc_ds/DSGen-software-code-3.2.0rc1/tools/"
+TARGET_DIR="/home/dionisius/bdma/ulb/data_warehouses/TPC-DS/workspace/sc_${SCALE_FACTOR}"
+DSDGEN_PATH="/home/dionisius/bdma/ulb/data_warehouses/TPC-DS/DSGen-software-code-3.2.0rc1/tools/"
 
+# There is a `delimiter` argument as well, but the default `|` works just fine.
+OPTIONS="-scale ${SCALE_FACTOR} -dir ${TARGET_DIR} -terminate N -force Y -parallel 4"
+
+echo "Writing data to ${TARGET_DIR}"
 mkdir -p ${TARGET_DIR}
 pushd ${DSDGEN_PATH}
 
-./dsdgen -scale ${SCALE_FACTOR} -dir ${TARGET_DIR} -parallel 4 -child 1 &
-./dsdgen -scale ${SCALE_FACTOR} -dir ${TARGET_DIR} -parallel 4 -child 2 &
-./dsdgen -scale ${SCALE_FACTOR} -dir ${TARGET_DIR} -parallel 4 -child 3 &
-./dsdgen -scale ${SCALE_FACTOR} -dir ${TARGET_DIR} -parallel 4 -child 4
+./dsdgen ${OPTIONS} -child 4 &
+./dsdgen ${OPTIONS} -child 3 &
+./dsdgen ${OPTIONS} -child 2 &
+./dsdgen ${OPTIONS} -child 1
 
+echo "Done!"
 popd
